0d025653f3ec4cf00107f22b4235ed457875e9c5
aliang@instance-1-personalVM <aliang@instance-1-personalVM>
2020-06-21 21:19:33 +0000

    remote server and remote gcp cloud storage is set up

--- server/start_mlflow.sh
+++ server/start_mlflow.sh
@@ -1,6 +1,8 @@
 # Exmaple 1: Run server locally with all defaults
-# mlflow server
+# mlflow server --host 0.0.0.0:8888
+
 # Exmaple 2: Run server remotely on cloud
-# mlflow server --backend-store-uri /home/aliang/mlflow_runs --default-artifact-root /home/aliang/mlflow_artifacts --host 0.0.0.0:8888
+# mlflow server --default-artifact-root ./artifacts --host 0.0.0.0:8888
+
 # Example 3: Run Server remotely And store models on cloud storage
-mlflow server --backend-store-uri /home/aliang/mlflow_runs --default-artifact-root /home/aliang/mlflow_artifacts --host 0.0.0.0:8888
+mlflow server --default-artifact-root gs://mldev_bucket_0/mlflow_artifacts --host 0.0.0.0:8888
